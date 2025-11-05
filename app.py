09cc04f6dd3ce664517ec4c1b366d98c098c0c44
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-11-06 06:31:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 # ---------- CẤU HÌNH CLOUDINARY -----------
 # Option A: hardcode (thay bằng thông tin của bạn)
 cloudinary.config(
-    cloud_name="YOUR_CLOUD_NAME",
-    api_key="YOUR_API_KEY",
-    api_secret="YOUR_API_SECRET"
+    cloud_name="dma3eclgv",
+    api_key="118974677734641",
+    api_secret="8Dhe37EYtXQVaaPpCsDlRRZSrE4"
 )
 
 # Option B (thay thế): nếu bạn muốn dùng ENV vars thay hardcode,
